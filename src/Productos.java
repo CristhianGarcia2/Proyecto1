2ba41703c41261b9648c4665ddde483ecd41ec96
CristhianGarcia2 <cgarcia@uniempresarial.edu.co>
2026-09-03 03:40:08 -0500

    Se agrega en Productos.java los diferentes atributos de la clase madre con nombre, idproducto, precio, stock y su constructor.

---
+++ src/Productos.java
@@ -0,0 +1,15 @@
+public class Productos {
+
+    private String nombre;
+    private int idproducto;
+    private double precio;
+    private boolean stock;
+
+    public Productos(String nombre, int idproducto, double precio, boolean stock) {
+        this.nombre = nombre;
+        this.idproducto = idproducto;
+        this.precio = precio;
+        this.stock = stock;
+
+    }
+}
