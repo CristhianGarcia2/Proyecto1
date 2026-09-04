b614828339a012c39f580db05bfcf0f9042d436b
CristhianGarcia2 <cgarcia@uniempresarial.edu.co>
2026-09-03 21:55:19 -0500

    Se agregar constructor con el polimorfismo en 5 parámetros en la clase electronicos y se vuelve a implementar el constructor de polimorfismo para los 3 parámetros en la clase Productos.

--- src/electronicos.java
+++ src/electronicos.java
@@ -6,17 +6,23 @@ public class electronicos extends  Productos {
     private boolean tieneGarantia;
 
     public electronicos(String nombre, int idproducto, double precio, boolean stock, String fabricante,
-                        int voltaje, double consumoKwh, boolean tieneGarantia){
+                        int voltaje, double consumoKwh, boolean tieneGarantia) {
 
-        super( nombre,  idproducto,  precio,  stock);
+        super(nombre, idproducto, precio, stock);
         this.fabricante = fabricante;
         this.voltaje = voltaje;
         this.consumoKwh = consumoKwh;
         this.tieneGarantia = tieneGarantia;
 
-
-
     }
+    public electronicos(String nombre, int idproducto, double precio, String fabricante, int voltaje) {
+        super(nombre, idproducto, precio);
+        this.fabricante = fabricante;
+        this.voltaje = voltaje;
+        this.consumoKwh = 1.5;
+        this.tieneGarantia = true;
+    }
+
     public void encendio () {
 
         System.out.println("Se enciende");
