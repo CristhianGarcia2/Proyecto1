7dd87778d4544692185a886948730ab302d1d2ed
CristhianGarcia2 <cgarcia@uniempresarial.edu.co>
2026-09-03 23:40:50 -0500

    Se implementaron bloques de código en el Main para validar si la lectura inicial y la modificación mediante setters de atributos en las difrentes clases de electronicos, ropa y alimentos.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,30 @@
 public class Main {
+
     public static void main(String[] args) {
 
         electronicos televisor = new electronicos("Smart TV", 101, 1500.0, "Sony", 220);
         ropa camisa = new ropa("Camisa Formal", 201, 45.0, "Algodón", 2.0);
         alimentos yogurt = new alimentos("Yogurt Griego", 301, "Plástico", 500);
+
+        System.out.println("Demostracion de getters y setters de las diferentes clases");
+
+        //Electronicos
+
+        System.out.println("Fabricante inicial : " + televisor.getFabricante());
+        televisor.setFabricante("Samsung");
+        System.out.println("Fabricante modificado: " + televisor.getFabricante());
+
+        //Ropa
+
+        System.out.println("Material inicial : " + camisa.getMaterial());
+        camisa.setMaterial("Lino");
+        System.out.println("Material modificado: " + camisa.getMaterial());
+
+        //Alimentos
+
+        System.out.println("Empaque inicial: " + yogurt.getTipoEmpaque());
+        yogurt.setTipoEmpaque("Vidrio");
+        System.out.println("Empaque modificado: " + yogurt.getTipoEmpaque());
 
     }
 }
