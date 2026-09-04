40b4cd2a35dd0e82041ddf378d351736571fb2a8
CristhianGarcia2 <cgarcia@uniempresarial.edu.co>
2026-09-03 23:33:23 -0500

    Se crea la clase main y se comienza a instanciar los objetos utilizando los constructores con el polimorfismo.

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        electronicos televisor = new electronicos("Smart TV", 101, 1500.0, "Sony", 220);
+        ropa camisa = new ropa("Camisa Formal", 201, 45.0, "Algodón", 2.0);
+        alimentos yogurt = new alimentos("Yogurt Griego", 301, "Plástico", 500);
+
+    }
+}
